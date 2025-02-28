4815eb98b89f329e08d774c8f82441836cde2010
NickYang03 <nicholasandrew.yang@students.mq.edu.au>
2025-02-28 11:54:39 +1100

    Changed Circle to Diamond

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -6,14 +6,17 @@ uniform vec2 u_screenSize;  	// screen dimensions in pixels
 layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
-   vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   vec2 v = abs(p - vec2(0.5, 0.5));
-   float d = max(v.x);
-  
-   if (d < 0.1) {
-      o_colour = vec4(u_colour, 1);
+   vec2 p = gl_FragCoord.xy / u_screenSize; // Normalize pixel coordinates to [0,1]
+   vec2 center = vec2(0.5);
+   vec2 offset = p - center;
+   
+   // Calculate Manhattan distance from the center
+    float d = abs(offset.x) + abs(offset.y);
+   
+   if (d < 0.5) {
+      o_colour = vec4(u_colour, 1.0);
    }
    else {
-      o_colour = vec4(255,255,255,1); // WHITE
+      o_colour = vec4(1.0); // WHITE
    }
 }
